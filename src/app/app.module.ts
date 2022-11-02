3f479f7976c2b46dd9b1e52ef7c0983a1c3adb03
Daniel Kondabarov <drk3nlzux@relay.firefox.com>
2022-11-02 23:22:20 +0000

    Start work on the main page header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/main/header/header.component';
+import { MainComponent } from './pages/main/main.component';
+import { HomeButtonComponent } from './pages/main/home-button/home-button.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    HomeButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
